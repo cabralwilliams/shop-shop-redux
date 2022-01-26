b901c520f5beaad6fe8bdb151163d68946683f31
Cabral Williams <cabral.williams@gmail.com>
2022-01-26 16:34:05 -0600

    ran build from client folder

---
+++ client/build/precache-manifest.da310ef27c731c851a9a26b500af3297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea8d4658873bdf8d429aa3b913f4ff85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e305a3c387b0843664",
+    "url": "/static/css/main.ad1af64d.chunk.css"
+  },
+  {
+    "revision": "7d9503f9cc076db6bdaa",
+    "url": "/static/js/2.0ed89f24.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.0ed89f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e305a3c387b0843664",
+    "url": "/static/js/main.b5eeaed8.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
